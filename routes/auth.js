4ae399b1b23812407cdffc1737748425bda9cafe
shubhammt123 <128716658+shubhammt123@users.noreply.github.com>
2025-06-16 22:28:34 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -83,7 +83,7 @@ router.post('/login', async (req, res) => {
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: error});
   }
 });
 
